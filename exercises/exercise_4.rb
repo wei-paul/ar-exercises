8275cac4ef3b2013d013aa89f392d077cdd13ca9
wei-paul <paul.wei607@gmail.com>
2021-11-10 00:01:44 +0000

    created some stores/made some queries

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# @mens_stores = Store.where(mens_apparel: true)
+# @mens_stores.each do |store|
+#   puts "#{store.name}, #{store.annual_revenue}"
+# end
+
+@womens_stores = Store.where("annual_revenue < ?", 1000000).where(womens_apparel: true)
+@womens_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end 
+
